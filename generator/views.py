b1001c34c56ea0d70c35d6154cdf08df92b00f3a
Luis Salgado <luis_salgado1999@outlook.com>
2022-10-19 18:26:30 -0500

    Initial commit

---
+++ generator/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, "pages/home.html")
+
+def about(request):
+    return render(request, "pages/about.html")
+
+def link(request):
+    return render(request, "pages/link.html")
+
+def think(request):
+    return render(request, "pages/think.html")
